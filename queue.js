da1a7629d406e9233a626c91ebc0fdcc424645b7
Matheus Franco <matheusefranco@gmail.com>
2023-03-15 09:32:43 -0300

    Aula2

--- queue.js
+++ queue.js
@@ -3,16 +3,31 @@ class Fila{
         this.maximo = maxSize;
         this.itens = [];
      }
-
+//---------------------------------------------
      enqueue(elemento){
         if(this.itens.length === this.maximo)
             return false;
+        //this.fim++;
+        //this.itens[this.fim] = elemento;    
         this.itens.push(elemento);// insere no final
         return true;    
      }
+
      dequeue(){
         let valor = this.itens.shift();
         return valor;
      }
+
+     isEmpty(){
+     /* if(this.itens.length == 0)
+         return true;
+      else
+         return false;  */ 
+       return this.itens.length === 0;
+     }
+
+     toString(){
+       return this.itens.toString();
+     }
     
-}
+}// fim classe 
