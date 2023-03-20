092e8703fd75fbc8690950b01a61bfe738b4b6ff
Matheus Franco <matheusefranco@gmail.com>
2023-03-20 08:56:16 -0300

    20-03

--- script.js
+++ script.js
@@ -5,11 +5,17 @@ function addFila(){
     const novo = 
         document.getElementById("novoElemento");
    if(minhaFila.enqueue(novo.value)===true){
-    alert("Dado inserido");
     novo.value="";
     novo.focus(); // volta o foco para o obj
-    console.log(minhaFila);
+    mostrarFila();
    }//
    else
     alert("Fila cheia!");
+}
+//--------------------------------------------------    
+function mostrarFila(){
+    const filaElemento = 
+       document.getElementById("listFila");
+    filaElemento.textContent = 
+        minhaFila.toString();   
 }
